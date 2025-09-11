ecda1e97864459b2d7f6394785123fc5547d3ab0
DrWallace <aqsaherryprasetya@gmail.com>
2025-09-11 15:17:15 +0700

    Add Praktikum 1 documentation and source code for Week 3

---
+++ Tugas Mobile Week3/SRC/Praktikum1Codelabs3.dart
@@ -0,0 +1,22 @@
+
+void main() {
+	String test = "test2";
+
+	if (test == "test1") {
+		print("Test1");
+	} else if (test == "test2") {
+		print("Test2");
+	} else {
+		print("Something else");
+	}
+
+	if (test == "test2") {
+		print("Test2 again");
+	}
+
+	String testValue = "true";
+	if (testValue == "true") {
+		print("Kebenaran");
+	}
+
+}
