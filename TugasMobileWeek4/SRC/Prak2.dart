b05d36707ba95036a798005a999a9e191837facd
DrWallace <aqsaherryprasetya@gmail.com>
2025-09-18 15:25:07 +0700

    Add Praktikum 2 source code and images; update README with explanations for steps 1 to 3

---
+++ TugasMobileWeek4/SRC/Prak2.dart
@@ -0,0 +1,13 @@
+void main() {
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+print(halogens);
+var names1 = <String>{};
+Set<String> names2 = {}; // This works, too.
+
+names1.add("Aqsa Herry Prastyo");
+names2.addAll(["2341720153"]);
+
+print(names1);
+print(names2);
+
+}
