d66e78a8a37f851ad0af92716c2bcde7e1f4f6bb
DrWallace <aqsaherryprasetya@gmail.com>
2025-09-22 15:06:57 +0700

    Add Praktikum 4 source code and images; update README with explanations for steps 1 to 3

---
+++ TugasMobileWeek4/SRC/Prak3.dart
@@ -0,0 +1,36 @@
+void main() {
+  var gifts = {
+  // Key:    Value
+  'first': 'partridge',
+  'second': 'turtledoves',
+  'fifth': 1
+};
+
+var nobleGases = {
+  2: 'helium',
+  10: 'neon',
+  18: 2,
+};
+
+print(gifts);
+print(nobleGases);
+
+  var mhs1 = Map<String, String>();
+  mhs1['nama'] = 'Herry';
+  mhs1['nim'] = '2341720153';
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+
+  var mhs2 = Map<int, String>();
+  mhs2[1] = 'Prastyo';
+  mhs2[2] = '2341720153';
+  nobleGases[2] = 'helium';
+  nobleGases[10] = 'neon';
+  nobleGases[18] = 'argon';
+
+  print(mhs1);
+  print(mhs2);
+  print(gifts);
+  print(nobleGases);
+}
