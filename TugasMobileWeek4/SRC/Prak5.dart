9884b9f41033a41c3dbeb70391f53c5916e209a8
DrWallace <aqsaherryprasetya@gmail.com>
2025-09-25 06:21:06 +0700

    Add Praktikum 5 source code and images; update README with explanations for steps 1 to 5

---
+++ TugasMobileWeek4/SRC/Prak5.dart
@@ -0,0 +1,19 @@
+(int, int) tukar((int, int) record) {
+  var (a, b) = record;
+  return (b, a);
+}
+void main() {
+  var tukar = ('first', a: 2, b: true, 'last');
+print(tukar);
+
+(String, int) mahasiswa;
+mahasiswa = ('Herry', 2341720153);
+print(mahasiswa);
+
+var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+print(mahasiswa2.$1); // Prints 'first'
+print(mahasiswa2.a); // Prints 2
+print(mahasiswa2.b); // Prints true
+print(mahasiswa2.$2); // Prints 'last'
+}
